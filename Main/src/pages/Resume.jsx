bc00e01096b8c6b81999bfca2894a4a4cfd5b19b
Anna T. <annangoctram1@gmail.com>
2025-01-18 23:22:26 -0500

    (feat) Add functionality to Resume page

--- Main/src/pages/Resume.jsx
+++ Main/src/pages/Resume.jsx
@@ -1,18 +1,54 @@
+/** @format */
+
+import myResume from "../assets/resume-image.png";
+
+const styles = {
+  span: {
+    fontWeight: "600",
+  },
+  heading: {
+    lineHeight: "0%",
+    fontSize: "1.8em",
+    color: "black",
+    paddingTop: "24px",
+  },
+  ul: {
+    fontSize: "1.4em",
+  },
+  list: {
+    marginBottom: ".8em",
+    listStyleType: "square",
+  },
+};
+
 export default function Resume() {
   return (
-    <div className="page-section">
-      <div className="page-layout">
-      <h1>RESUME</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus
-          cursus in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit
-          at.
-        </p>
+    <div className="page-wrapper">
+      <div className="skills-text">
+        <div className="column">
+          <h1 style={styles.heading}>Technical</h1>
+          <ul style={styles.ul}>
+            <li style={styles.list}>
+              <span style={styles.span}>Front-End:</span> HTML, CSS, JavaScript,
+              TypeScript, React, Responsive Design
+            </li>
+            <li style={styles.list}>
+              <span style={styles.span}>Back-End:</span> Intro to server-side
+              developmeent, APIs
+            </li>
+            <li style={styles.list}>
+              <span style={styles.span}>Frameworks & Libraries:</span> React,
+              Angular, Express.js, Vite
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="row">
+        <img
+          src={myResume}
+          className="resume-img"
+        />
       </div>
     </div>
   );
-  }
+}
